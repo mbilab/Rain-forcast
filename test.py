ec12d3fc7117cd6b072ed4bda6875212e1c472e0
yichung279 <ql3895619@gmail.com>
2018-02-04 12:04:23 +0800

    make test.py easier to use

--- test.py
+++ test.py
@@ -21,21 +21,22 @@ if __name__ == '__main__':
     _10mins = timedelta(seconds=600)
     file_datetime = begin 
     
-    fnt = ImageFont.truetype('zh.ttf', 6)
+    fnt = ImageFont.truetype('zh.ttf', 10)
     font_color = 250
-    base = config['path']+"image/CV1_3600_"+ end.strftime("%Y%m%d%H%M") + ".png"
+    base = config['path']+ end.strftime("%Y%m%d%H%M") + ".png"
     base_im = Image.open(base).crop((x - radius, y - radius, x + radius, y + radius))
     
     while (file_datetime < end) :
         stamp =  file_datetime.strftime("%Y%m%d%H%M")
-        filename = config['path']+"image/CV1_3600_"+ stamp + ".png"
-        centr =centroid ( stamp , x , y  )
+        filename = config['path']+ stamp + ".png"
+        centr =centroid ( filename , x , y  )
         if centr[0] != 0 :
-            ImageDraw.Draw( base_im ).text( (centr[0]-2,centr[1]- 3 ),'@'+filename, font=fnt ,fill = (font_color ,0 , 0 ) )
+            ImageDraw.Draw( base_im ).text( (centr[0]-3,centr[1]- 5 ),'@'+stamp, font=fnt ,fill = (font_color ,0 , 0 ) )
         if font_color > 0:
-            font_color -= 30
+            font_color -= 10
         file_datetime = file_datetime +_10mins
-        base_im.save('pub/centroid_test.png')
+        base_im.save('pub/'+end.strftime("%Y%m%d%H%M")+'.png')
+    print (config['path_save']+'pub/'+end.strftime("%Y%m%d%H%M")+'.png')
 
 
 
