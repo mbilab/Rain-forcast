838313f23bc542de45a0dfc9219175d03cfdf692
dirty <darby.chang@gmail.com>
2018-02-12 23:22:34 +0800

    working in wind_direction()

--- rain.py
+++ rain.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from PIL import Image,ImageDraw,ImageFont
+from PIL import Image, ImageDraw, ImageFont
 import argparse
 import math
 import json
@@ -32,15 +32,11 @@ def arrow_angle(vector):
 def arrow_positoin(angle): # 修改時注意有座標轉換議題
     return [int(radius - 80 * math.cos(angle) - 50), int(radius + 80 * math.sin(angle) - 50)]
 
-def centroid(filename, position_x, position_y):
-    image = Image.open(filename).crop((position_x - radius, position_y - radius, position_x + radius, position_y + radius))
-    width, height = image.size#xy
+def centroid(image):
+    count = 0; x = 0; y= 0
     pixels = image.load()
-    count = 0
-    x = 0
-    y = 0
+    width, height = image.size
 
-    # calculate centroid
     for i in range(width):
         for j in range(height):
             weight = color_weight(pixels[i, j])
@@ -48,10 +44,10 @@ def centroid(filename, position_x, position_y):
             x += weight * i
             y += weight * j
 
-
     if count > 0:
         x /= count
         y /= count
+
     return x, y
 
 def color_weight(color):
@@ -93,11 +89,12 @@ def rain_area(pixels, x, y):#True: In distance<35 ,more than 50% of pixels are r
     else:
         return False
 
-def moving_direction(pixels, after, before):
-    if before == [0, 0]:#剛發展的雲 為0向量
-        return before
-    else:
-        #!TODO: improve precision
+def wind_direction(centroids, image):
+    print(image)
+    exit()
+    if [0, 0] == centroids[-2]: # new rain in the last image (no rain in images[-2])
+        return 0, 0 # the wind direction is (0, 0)
+    else: #! TODO: better logic
         count_cloud = 0
         for i in range(radius * 2):
             for j in range(radius * 2):
@@ -113,27 +110,21 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
 
     parser.add_argument('images', help='image filenames in YYYYmmDDHHMM format', nargs='*')
-    parser.add_argument('-x', default=1675, help='x on the rader graph of CWB, NCKU is at (1675, 1475)')
-    parser.add_argument('-y', default=1475, help='y on the rader graph of CWB, NCKU is at (1675, 1475)')
+    parser.add_argument('-r', default=150, help='radius on the rader graph of CWB (default: 150)', type=int)
+    parser.add_argument('-x', default=1675, help='x on the rader graph of CWB, NCKU is at (1675, 1475)', type=int)
+    parser.add_argument('-y', default=1475, help='y on the rader graph of CWB, NCKU is at (1675, 1475)', type=int)
     args = parser.parse_args()
 
-    before_filename = "image/CV1_3600_" + args.images[0] + ".png"
-    after_filename  = "image/CV1_3600_" + args.images[1] + ".png"
-    position_x = int(args.x)
-    position_y = int(args.y)
+    area = (args.x - args.r, args.y - args.r, args.x + args.r, args.y + args.r)
+    images = [Image.open('image/CV1_3600_%s.png' % v).crop(area) for v in args.images]
+    centroids = [centroid(v) for v in images]
 
-    before = centroid(before_filename, position_x, position_y)
-    after = centroid(after_filename, position_x, position_y)
-
-    #may no rain
-    if after[0] == 0:
-        print(False) #no rain in image after
-
+    if [0, 0] == centroids[-1]:
+        print(False) # no rain in the last image
     else: #crop image in size of 300 px*300 px ,calculate simply
-        base_im = Image.open(after_filename).crop((position_x - radius, position_y - radius, position_x + radius, position_y + radius))
-        pixels = base_im.load() #calculate in pixels
+        vector = wind_direction(centroids, images[-1])
+        exit()
 
-        vector = moving_direction(pixels, after, before)
         #pixels[x,y] will come to center 10 minutes later
         x = radius - 2 * vector[0]
         y = radius - 2 * vector[1]
